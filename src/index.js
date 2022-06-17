9ddcc0e52906978fd9ac3aa4ccc6db4d05e1a2f9
Pedro Santos <phsantosti@gmail.com>
2022-06-17 20:08:14 -0300

    Create README.md

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 const express = require('express');
+const {v4: uuidv4 } = require("uuid");
 
 const app = express();
+app.use(express.json());
 
+const customers = [];
+/**
+ * document = string,
+ * name = string
+ * id = uid,
+ * statement = []
+ */
+app.post("/account", (request, response) => {
+   const {name, document} = request.body;
+   const id = uuidv4();
 
+   customers.push({
+      id: id,
+      name: name,
+      document: document,
+      statement: []
+   });
+
+   return response.status(201).send();
+});
 
 app.listen(3333);
